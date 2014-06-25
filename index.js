896265c0e7b532275f592a86bf48ddfad8c16ffd
Alex Kocharin <alex@kocharin.ru>
2014-06-25 16:34:35 +0400

    catch requests to server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var ClientRequest = require('http').ClientRequest
-var EventEmitter = require('events').EventEmitter
-var util = require('util')
+  , Server        = require('http').Server
+  , EventEmitter  = require('events').EventEmitter
+  , util          = require('util')
 
 module.exports.catch_outgoing_http = function(callback) {
 	function get_ee() {
 		if (!this._expose_curl_ee) {
 			this._expose_curl_ee = new EventEmitter()
 			callback(this._expose_curl_ee)
-			this._expose_curl_ee.emit('headers')
+
+			var headers = {}
+			for (var h in this._headers) {
+				headers[this._headerNames[h]] = this._headers[h]
+			}
+			this._expose_curl_ee.emit('headers', headers)
 		}
 		return this._expose_curl_ee
 	}
@@ -26,12 +32,29 @@ module.exports.catch_outgoing_http = function(callback) {
 }
 
 module.exports.catch_incoming_http = function(callback) {
-/*	var oldserver = _http_server.ServerResponse
-	_http_server.ServerResponse = function(req) {
-		console.log()
+	var old_al = Server.prototype.addListener
+	Server.prototype.addListener = Server.prototype.on = function(event, listener) {
+		if (event === 'request' && !this._expose_curl_req) {
+			this._expose_curl_req = 1
+			old_al.call(this, 'request', request_listener)
+		}
+		old_al.apply(this, arguments)
 	}
-	for (var x in oldserver) {
-		_http_server.ServerResponse[x] = oldserver[x]
-	}*/
+
+	function request_listener(req, res) {
+		var ee = new EventEmitter()
+		callback(ee)
+
+		var old_emit = req.emit
+		req.emit = function(event, data) {
+			old_emit.apply(this, arguments)
+			if (event === 'data') {
+				ee.emit(event, data, this._readableState.encoding)
+			} else if (event === 'end') {
+				ee.emit('end')
+			}
+		}
+		ee.emit('headers', req.headers)
+	}
 }
 
